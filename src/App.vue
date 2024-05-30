81805caedd5fda2e621a6d9745ef738a5d502030
simone-nolberto <simone.nolberto@outlook.com>
2024-05-30 15:47:49 +0200

    update with workin ajax call from API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <script>
+import axios from "axios";
 
+export default {
+  name: "App",
+
+  data() {
+    return {
+      base_api_url: "http://127.0.0.1:8000",
+      base_projects_url: "/api/projects",
+
+      projects: [],
+      loading: true,
+    };
+  },
+
+  methods: {
+    callAPI(url) {
+      axios
+        .get(url)
+        .then((response) => {
+          // console.log(response);
+          this.projects = response.data.projects;
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+
+  mounted() {
+    let url = this.base_api_url + this.base_projects_url;
+
+    this.callAPI(url);
+  },
+};
 </script>
 
-<template>
-  
-</template>
+<template></template>
 
-<style>
-
-</style>
+<style></style>
