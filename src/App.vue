c187b18a7adf745c8e1b7e15625778dbd63aee2a
simone-nolberto <simone.nolberto@outlook.com>
2024-05-30 22:09:21 +0200

    update with basic pagination function

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,52 @@ export default {
 </script>
 
 <template>
-  <ProjectCard v-for="project in this.state.projects" :project="project"></ProjectCard>
+  <header>Header</header>
+
+  <div class="container">
+    <div class="row">
+      <ProjectCard
+        v-for="project in this.state.projects.data"
+        :project="project"
+      ></ProjectCard>
+    </div>
+  </div>
+
+  <div class="container">
+    <nav aria-label="Page navigation">
+      <ul class="pagination">
+        <li class="page-item" v-show="this.state.projects.prev_page_url != null">
+          <button
+            class="page-link"
+            href="#"
+            @click="this.state.getProjects(this.state.projects.prev_page_url)"
+            aria-label="Previous"
+          >
+            <span aria-hidden="true">&laquo;</span>
+          </button>
+        </li>
+        <!-- <li class="page-item active">
+          <a class="page-link" href="#">1</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li> -->
+        <li class="page-item">
+          <button
+            class="page-link"
+            href="#"
+            @click="this.state.getProjects(this.state.projects.next_page_url)"
+            aria-label="Next"
+          >
+            <span aria-hidden="true">&raquo;</span>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <footer>Footer</footer>
 </template>
 
 <style></style>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,20 +13,16 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <div class="card" style="width: 18rem">
-          <div class="card-body">
-            <h5 class="card-title">{{ this.project.project_title }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">
-              Created by: {{ this.project.author }}
-            </h6>
-            <p class="card-text">
-              {{ this.project.description }}
-            </p>
-          </div>
-        </div>
+  <div class="col">
+    <div class="card" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">{{ this.project.project_title }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">
+          Created by: {{ this.project.author }}
+        </h6>
+        <p class="card-text">
+          {{ this.project.description }}
+        </p>
       </div>
     </div>
   </div>
